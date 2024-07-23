c55549ea04896ebc8cd2a5f90a167531dd6f4ffa
3bdalah <3bdallah.dev@gmail.com>
2024-07-23 23:04:21 +0300

    configuer app routing app module with other modulse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'movies', loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule) },
+  { path: '**', component: NotFoundComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
