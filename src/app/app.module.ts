c5607f6674124a5fa77f384d86e804b55a36f14d
3bdalah <3bdallah.dev@gmail.com>
2024-07-23 23:06:06 +0300

    update app model import shared and movies and auth models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { MoviesModule } from './movies/movies.module';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule 
+    AppRoutingModule,
+    SharedModule,
+    MoviesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
