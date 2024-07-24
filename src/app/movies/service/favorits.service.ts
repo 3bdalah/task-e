9a2c01e18679c834919c109d39e189ff1e009955
3bdalah <3bdallah.dev@gmail.com>
2024-07-24 10:54:57 +0300

    feat: built servics favorits

--- src/app/movies/service/favorits.service.ts
+++ src/app/movies/service/favorits.service.ts
@@ -4,6 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FavoritsService {
+  private localKey = 'favorites';
 
-  constructor() { }
+  getFavorites(): any[] {
+    return JSON.parse(localStorage.getItem(this.localKey) || '[]');
+  }
+
+  isFavorite(movieId: number): boolean {
+    const favorites = this.getFavorites();
+    return favorites.some(movie => movie.id === movieId);
+  }
+
+  addFavorite(movie: any): void {
+    const favorites = this.getFavorites();
+    favorites.push(movie);
+    localStorage.setItem(this.localKey, JSON.stringify(favorites));
+  }
+
+  removeFavorite(movieId: number): void {
+    let favorites = this.getFavorites();
+    favorites = favorites.filter(movie => movie.id !== movieId);
+    localStorage.setItem(this.localKey, JSON.stringify(favorites));
+  }
 }
