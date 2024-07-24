76287d1a86bcfbc7e8dc9e915a267d09c7a6d454
3bdalah <3bdallah.dev@gmail.com>
2024-07-24 11:05:34 +0300

    feat: built component movie details and styled page

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Movie } from '../interface/movie';
+import { CommentsService } from '../service/comments.service';
+import { FavoritsService } from '../service/favorits.service';
+import { MoviesService } from '../service/movies.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,5 +11,63 @@ import { Component } from '@angular/core';
   styleUrl: './movie-details.component.css'
 })
 export class MovieDetailsComponent {
+  movieAllDetailes!: Movie;
+  moveiId!: number;
+  comments: any[] = [];
+  newComment: string = '';
+  newReplies: { [key: number]: string } = {};
+constructor(private router:Router,private  _ActivateRoute:ActivatedRoute,private fullyDataMovie:MoviesService,private favoriteService: FavoritsService,
+  private commentService: CommentsService){}
+  ngOnInit(){
+    const id = this._ActivateRoute.snapshot.params['id'];
 
+    this._ActivateRoute.params.subscribe((params) => { this.moveiId = params['id'];
+      this.fullyDataMovie
+        .getfullyDataMovie(this.moveiId)
+        .subscribe((dataMovie) => {  this.movieAllDetailes = dataMovie;});
+        console.log("all data about mmovie ", this.movieAllDetailes);
+      });
+      this.loadComments(id);
+  
+  }
+
+  loadComments(movieId: number): void {
+    this.comments = this.commentService.getComments(movieId);
+  }
+
+  addComment(): void {
+    if (this.newComment.trim()) {
+      const comment = {
+        text: this.newComment,
+        date: new Date()
+      };
+      this.commentService.addComment(this.movieAllDetailes.id, comment);
+      this.loadComments(this.movieAllDetailes.id);
+      this.newComment = '';
+    }
+  }
+
+
+  addReply(commentIndex: number): void {
+    const replyText = this.newReplies[commentIndex];
+    if (replyText && replyText.trim()) {
+      const reply = {
+        text: replyText,
+        date: new Date()
+      };
+      this.commentService.addReply(this.movieAllDetailes.id, commentIndex, reply);
+      this.loadComments(this.movieAllDetailes.id);
+      this.newReplies[commentIndex] = '';
+    }
+  }
+  isFavorite(): boolean {
+    return this.favoriteService.isFavorite(this.movieAllDetailes.id);
+  }
+  toggleFavorite(): void {
+    if (this.isFavorite()) {
+      this.favoriteService.removeFavorite(this.movieAllDetailes.id);
+    } else {
+      this.favoriteService.addFavorite(this.movieAllDetailes);
+    }
+  }
 }
