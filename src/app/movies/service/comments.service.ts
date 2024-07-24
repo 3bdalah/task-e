51ea4d27c96123a60820a2f14183c53b785e732d
3bdalah <3bdallah.dev@gmail.com>
2024-07-24 10:50:16 +0300

    feat: handled service comments

--- src/app/movies/service/comments.service.ts
+++ src/app/movies/service/comments.service.ts
@@ -4,6 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CommentsService {
+  private localKey = 'comments';
 
-  constructor() { }
+  getComments(movieId: number): any[] {
+    const comments = JSON.parse(localStorage.getItem(this.localKey) || '{}');
+    return comments[movieId] || [];
+  }
+  addComment(movieId: number, comment: any): void {
+    const comments = JSON.parse(localStorage.getItem(this.localKey) || '{}');
+    if (!comments[movieId]) {
+      comments[movieId] = [];
+    }
+    comments[movieId].push(comment);
+    localStorage.setItem(this.localKey, JSON.stringify(comments));
+  }
+  addReply(movieId: number, commentIndex: number, reply: any): void {
+    const comments = JSON.parse(localStorage.getItem(this.localKey) || '{}');
+    if (comments[movieId] && comments[movieId][commentIndex]) {
+      if (!comments[movieId][commentIndex].replies) {
+        comments[movieId][commentIndex].replies = [];
+      }
+      comments[movieId][commentIndex].replies.push(reply);
+      localStorage.setItem(this.localKey, JSON.stringify(comments));
+    }
+  }
 }
